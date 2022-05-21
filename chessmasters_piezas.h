c9ec3665895f999fd13e7acc4e85fde0804d5190
AlbertoGasco <99816053+AlbertoGasco@users.noreply.github.com>
2022-05-21 13:17:23 +0200

    libreria piezas

---
+++ chessmasters_piezas.h
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int peones (int *i_inicial, int *j_inicial, int *i_final, int *j_final, char tablero[8][8]){
+    
+    return 1;
+}
+
+int caballos (int *i_inicial, int *j_inicial, int *i_final, int *j_final, char tablero[8][8]){
+    
+    return 1;
+}
+
+int alfiles (int *i_inicial, int *j_inicial, int *i_final, int *j_final, char tablero[8][8]){
+    
+    return 1;
+}
+
+int torres (int *i_inicial, int *j_inicial, int *i_final, int *j_final, char tablero[8][8]){
+    
+    return 1;
+}
+
+int reyes (int *i_inicial, int *j_inicial, int *i_final, int *j_final, char tablero[8][8]){
+    
+    return 1;
+}
+
+int damas (int *i_inicial, int *j_inicial, int *i_final, int *j_final, char tablero[8][8]){
+    
+    return 1;
+}
+
+
