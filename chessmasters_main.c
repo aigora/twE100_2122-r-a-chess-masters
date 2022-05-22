4d024a9206f7b1daf8038f7c26c1a3b9216e01f7
AlbertoGasco <99816053+AlbertoGasco@users.noreply.github.com>
2022-05-22 18:47:27 +0200

    updates

--- chessmasters_main.c
+++ chessmasters_main.c
@@ -2,38 +2,50 @@
 #include "chessmasters_funciones.h"
 #include "chessmasters_piezas.h"
 
-void mover_pieza(int *i_inicial, int *j_inicial, int *i_final,   int *j_final, char tablero[8][8]);
+void mover_pieza(int i_inicial, int j_inicial, int i_final,   int j_final, char tablero[8][8]);
 
 void introduce_coordenadas(int *i_inicial, int *j_inicial, int *i_final,   int *j_final, int *contador_movimientos);
 
 int main() {
-    int i_0,j_0,i_1,j_1,contador=0;
+    int i_0,j_0,i_1,j_1,contador=0,fin_partida=0;
     char tablero[8][8];
     tablero_inicial(tablero);
     imprimir_tablero(tablero);
-    introduce_coordenadas(&i_0,&j_0,&i_1,&j_1,&contador);
-     
-    
+    do{
+        introduce_coordenadas(&i_0,&j_0,&i_1,&j_1,&contador);
+        if (comprobar==0){
+            mover_pieza(i_0,j_0,i_1,j_1,tablero);
+        }else{
+            printf("Introduzca un movimiento valido");
+        }
+
+    }while(fin_partida==0);
+
+
+
     return 0;
 }
 
-void mover_pieza(int *i_inicial, int *j_inicial, int *i_final,   int *j_final, char tablero[8][8]) {
-    tablero[*i_final][*j_final] = tablero[*i_inicial][*j_inicial];
-    tablero[*i_inicial][*j_inicial] = '.';
+void mover_pieza(int i_inicial, int j_inicial, int i_final,   int j_final, char tablero[8][8]) {
+    tablero[i_final][j_final] = tablero[i_inicial][j_inicial];
+    tablero[i_inicial][j_inicial] = '.';
 }
 
 void introduce_coordenadas(int *i_inicial, int *j_inicial, int *i_final, int *j_final, int *contador_movimientos) {
     int letra_inicial,numero_inicial,letra_final,numero_final;
-    *contador_movimientos++;
+    (*contador_movimientos)++;
     if ((*contador_movimientos%2)==0) {
         printf("\n\tTurno de piezas NEGRAS\n");
-    }else{
+    }
+    else{
         printf("\n\tTurno de piezas BLANCAS\n");
     }
     printf("\nIntroduzca coordenadas de la pieza a mover (letra y numero):");
     scanf("%c",&letra_inicial);
     scanf(" %d",&numero_inicial);
+
     printf("Introduzca las coordenadas donde quiere mover la pieza:");
     scanf("%c",&letra_final);
     scanf(" %d",&numero_final);
+
 }
