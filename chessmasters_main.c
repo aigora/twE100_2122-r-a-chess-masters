a2b98183063e0f326e54ed4cd31dd24a3d93f8f8
AlbertoGasco <99816053+AlbertoGasco@users.noreply.github.com>
2022-05-22 17:26:06 +0200

    Update chessmasters_main.c

--- chessmasters_main.c
+++ chessmasters_main.c
@@ -2,9 +2,9 @@
 #include "chessmasters_funciones.h"
 #include "chessmasters_piezas.h"
 
-void mover_pieza(int *i_inicial, int *j_inicial, int *i_final,   int *j_final);
+void mover_pieza(int *i_inicial, int *j_inicial, int *i_final,   int *j_final, char tablero[8][8]);
 
-void introduce_coordenadas(int *i_inicial, int *j_inicial, int *i_final,   int *j_final);
+void introduce_coordenadas(int *i_inicial, int *j_inicial, int *i_final,   int *j_final, int *contador_movimientos);
 
 int main() {
     int i_0,j_0,i_1,j_1,contador=0;
@@ -17,8 +17,8 @@ int main() {
     return 0;
 }
 
-void mover_pieza(int *i_inicial, int *j_inicial, int *i_final,   int *j_final) {
-    tablero[*i_finall][*j_final] = tablero[*i_inicial][*j_inicial];
+void mover_pieza(int *i_inicial, int *j_inicial, int *i_final,   int *j_final, char tablero[8][8]) {
+    tablero[*i_final][*j_final] = tablero[*i_inicial][*j_inicial];
     tablero[*i_inicial][*j_inicial] = '.';
 }
 
