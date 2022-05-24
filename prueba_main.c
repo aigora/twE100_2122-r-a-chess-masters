8a6567e691b1f51dbc39ba69f6fb1a91005d106a
roprandi11 <ricardo.oprandi@alumnos.upm.es>
2022-05-24 14:17:34 +0200

    Update prueba_main.c

--- prueba_main.c
+++ prueba_main.c
@@ -318,32 +318,32 @@ int peones (int *i_inicial, int *j_inicial, int *i_final, int *j_final, char tab
     
     if ((tablero[*i_inicial][*j_inicial])=='P') { //peon negras 4 movimientos posibles
         
-        if ((tablero[*i_final][*j_final])==(tablero[*i_inicial][*j_inicial+1])) {
-            if ((*j_final<8)&&((tablero[*i_final][*j_final])=='.')) {
+        if ((tablero[*i_final][*j_final])==(tablero[*i_inicial+1][*j_inicial])) { //avanza 1
+            if ((*i_final<8)&&((tablero[*i_final][*j_final])=='.')) {
                 return 0;
             }else{
                 return 1;
             }
         }
         
-        if ((tablero[*i_final][*j_final])==(tablero[*i_inicial][*j_inicial+2])) {
-            if ((*j_inicial==1)&&((tablero[*i_final][*j_final])=='.')) {
+        if ((tablero[*i_final][*j_final])==(tablero[*i_inicial+2][*j_inicial])) { //avanza 2
+            if ((*i_inicial==1)&&((tablero[*i_final][*j_final])=='.')) {
                 return 0;
             }else{
                 return 1;
             }
         }
         
-        if ((tablero[*i_final][*j_final])==(tablero[*i_inicial-1][*j_inicial+1])) {
-            if ((*j_final<8)&&((tablero[*i_final][*j_final])>='a')&&(0<=(*i_final))) {
+        if ((tablero[*i_final][*j_final])==(tablero[*i_inicial+1][*j_inicial-1])) { //come hacia su derecha
+            if ((*i_final<8)&&((tablero[*i_final][*j_final])>='a')&&(0<=(*j_final))) {
                 return 0;
             }else{
                 return 1;
             }
         }
         
-        if ((tablero[*i_final][*j_final])==(tablero[*i_inicial+1][*j_inicial+1])) {
-            if ((*j_final<8)&&((tablero[*i_final][*j_final])>='a')&&(*i_final<8)) {
+        if ((tablero[*i_final][*j_final])==(tablero[*i_inicial+1][*j_inicial+1])) { //come hacia su izquierda
+            if ((*i_final<8)&&((tablero[*i_final][*j_final])>='a')&&(*j_final<8)) { //limites tablero y pieza minuscula
                 return 0;
             }else{
                 return 1;
@@ -354,16 +354,16 @@ int peones (int *i_inicial, int *j_inicial, int *i_final, int *j_final, char tab
     
     if ((tablero[*i_inicial][*j_inicial])=='p') {//peon blancas
         
-        if ((tablero[*i_final][*j_final])==(tablero[*i_inicial][*j_inicial-1])) {
-            if ((*j_final>=0)&&((tablero[*i_final][*j_final])=='.')) {
+        if ((tablero[*i_final][*j_final])==(tablero[*i_inicial-1][*j_inicial])) {
+            if ((*i_final>=0)&&((tablero[*i_final][*j_final])=='.')) {
                 return 0;
             }else{
                 return 1;
             }
         }
         
-        if ((tablero[*i_final][*j_final])==(tablero[*i_inicial][*j_inicial-2])) {
-            if ((*j_inicial==6)&&((tablero[*i_final][*j_final])=='.')) {
+        if ((tablero[*i_final][*j_final])==(tablero[*i_inicial-2][*j_inicial])) {
+            if ((*i_inicial==6)&&((tablero[*i_final][*j_final])=='.')) {
                 return 0;
             }else{
                 return 1;
@@ -371,15 +371,15 @@ int peones (int *i_inicial, int *j_inicial, int *i_final, int *j_final, char tab
         }
         
         if ((tablero[*i_final][*j_final])==(tablero[*i_inicial-1][*j_inicial-1])) {
-            if ((*j_final>=0)&&((tablero[*i_final][*j_final])>='A')&&((tablero[*i_final][*j_final])<='T')&&(0<=(*i_final))) {//limites y contiene mayuscula
+            if ((*i_final>=0)&&((tablero[*i_final][*j_final])>='A')&&((tablero[*i_final][*j_final])<='T')&&(0<=(*j_final))) {//limites y contiene mayuscula
                 return 0;
             }else{
                 return 1;
             }
         }
         
-        if ((tablero[*i_final][*j_final])==(tablero[*i_inicial+1][*j_inicial-1])) {
-            if ((*j_final>=0)&&((tablero[*i_final][*j_final])>='A')&&((tablero[*i_final][*j_final])<='T')&&(*i_final<8)) { // limites y contiene mayuscula
+        if ((tablero[*i_final][*j_final])==(tablero[*i_inicial-1][*j_inicial+1])) {
+            if ((*i_final>=0)&&((tablero[*i_final][*j_final])>='A')&&((tablero[*i_final][*j_final])<='T')&&(*j_final<8)) { // limites y contiene mayuscula
                 return 0;
             }else{
                 return 1;
